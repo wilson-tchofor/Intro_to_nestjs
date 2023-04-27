75983dd8907bba3aad2d563e59a6e4e3126f4e92
tchoforwilson@gmail.com <wilson-tchofor@idealbox.org>
2023-04-27 18:15:03 +0100

    Defines database schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/products.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule,  MongooseModule.forRoot(
+    'mongodb://localhost:27017/intro',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
