e307894b137ae524ea549d55a7ce3b41cf67464d
tchoforwilson@gmail.com <wilson-tchofor@idealbox.org>
2023-04-27 18:13:33 +0100

    define product model

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule, Schema } from "@nestjs/mongoose";
+import {Product, ProductSchema} from "./product.model";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
 
 @Module({
-    controllers: [ProductsController],
-    providers:[ProductsService]
+  imports: [MongooseModule.forFeature([{name:'Product', schema: ProductSchema}])],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
-export class ProductModule { };
+export class ProductModule {};
